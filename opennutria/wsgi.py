2a3bb260d59a5da6528ae0ece268dc4ac040d5af
lanzac <a.lanrezac@gmail.com>
2025-06-02 23:12:10 +0200

    Rename main app correctly

--- opennutria/wsgi.py
+++ opennutria/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nuto project.
+WSGI config for opennutria project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nuto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opennutria.settings')
 
 application = get_wsgi_application()
