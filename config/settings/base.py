33088a04b8370c365c07ab4368f68be4d8099a36
lanzac <a.lanrezac@gmail.com>
2025-09-10 01:03:25 +0200

    ✨ feat(api): add API router for user management and API documentation
    integrate user view set with Django REST framework using router
    configure API URL patterns in settings and add authentication endpoints

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,10 +83,10 @@ THIRD_PARTY_APPS = [
     "allauth.mfa",
     "allauth.socialaccount",
     "django_celery_beat",
-    # "rest_framework",
-    # "rest_framework.authtoken",
+    "rest_framework",
+    "rest_framework.authtoken",
     "corsheaders",
-    # "drf_spectacular",
+    "drf_spectacular",
     "webpack_loader",
 ]
 
@@ -333,27 +333,27 @@ SOCIALACCOUNT_FORMS = {"signup": "opennutrilab.users.forms.UserSocialSignupForm"
 # django-rest-framework
 # -------------------------------------------------------------------------------
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
-# REST_FRAMEWORK = {
-#     "DEFAULT_AUTHENTICATION_CLASSES": (
-#         "rest_framework.authentication.SessionAuthentication",
-#         "rest_framework.authentication.TokenAuthentication",
-#     ),
-#     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
-#     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-# }
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r"^/api/.*$"
 
 # # By Default swagger ui is available only to admin user(s). You can change permission classes to change that
 # # See more configuration options at https://drf-spectacular.readthedocs.io/en/latest/settings.html#settings
-# SPECTACULAR_SETTINGS = {
-#     "TITLE": "Open Nutri Lab API",
-#     "DESCRIPTION": "Documentation of API endpoints of Open Nutri Lab",
-#     "VERSION": "1.0.0",
-#     "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
-#     "SCHEMA_PATH_PREFIX": "/api/",
-# }
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Open Nutri Lab API",
+    "DESCRIPTION": "Documentation of API endpoints of Open Nutri Lab",
+    "VERSION": "1.0.0",
+    "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
+    "SCHEMA_PATH_PREFIX": "/api/",
+}
 # django-webpack-loader
 # ------------------------------------------------------------------------------
 WEBPACK_LOADER = {
--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include, path
+from django.urls import include
+from django.urls import path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
-
-# from drf_spectacular.views import SpectacularAPIView
-# from drf_spectacular.views import SpectacularSwaggerView
-# from rest_framework.authtoken.views import obtain_auth_token
+from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import SpectacularSwaggerView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),  # Language switching
@@ -36,15 +36,15 @@ if settings.DEBUG:
 # API URLS
 urlpatterns += [
     # API base url
-    # path("api/", include("config.api_router")),
+    path("api/", include("config.api_router")),
     # DRF auth token
-    # path("api/auth-token/", obtain_auth_token, name="obtain_auth_token"),
-    # path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
-    # path(
-    #     "api/docs/",
-    #     SpectacularSwaggerView.as_view(url_name="api-schema"),
-    #     name="api-docs",
-    # ),
+    path("api/auth-token/", obtain_auth_token, name="obtain_auth_token"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
 ]
 
 if settings.DEBUG:
