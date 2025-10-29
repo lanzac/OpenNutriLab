b748ac07df85c01aeee69193b6249cf42d2b619d
lanzac <a.lanrezac@gmail.com>
2025-10-29 23:51:16 +0000

    ♻️ refactor(forms): remove unused fetched_image_url initialization
    - eliminate fetched_image_url from extra_data as it is no longer needed

--- foods/forms.py
+++ foods/forms.py
@@ -54,10 +54,6 @@ class FoodForm(forms.ModelForm):
         self.extra_data: dict[str, str | None] | None = extra_data or {}
         super().__init__(*args, **kwargs)  # pyright: ignore[reportUnknownArgumentType]
 
-        self.fetched_image_url: str | None = (
-            self.extra_data.get("fetched_image_url") if self.extra_data else None
-        )
-
         # --------------------------------------------------------------------
         # FormHelper configuration
         # --------------------------------------------------------------------
