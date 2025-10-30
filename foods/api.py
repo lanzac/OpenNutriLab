35e6af513cbe476f074418c5e76bda6e4766b83b
lanzac <a.lanrezac@gmail.com>
2025-10-30 16:58:41 +0000

    ♻️ refactor(api): reorganize imports and update get_product function signature
    - move import statements for better readability
    - change get_product function to accept barcode as a parameter
    - improve error handling for fetching product data

--- foods/api.py
+++ foods/api.py
@@ -1,16 +1,20 @@
 # aliments/api.py
-from ninja import Router
-from .schema import ProductSchema
-from glom import glom
 import requests
+from ninja import Router
 from ninja.errors import HttpError
+
+from foods.schema import ProductSchema
+
 from .off_utils import fetch_product_data
 
 router = Router()
 
-@router.get("/off/{barcode}", response=ProductSchema)
-def get_product(request, barcode: str):
+
+@router.get(path="/off/{barcode}", response=ProductSchema)
+def get_product(barcode: str) -> ProductSchema:
     try:
         return fetch_product_data(barcode)
-    except Exception as e:
-        raise HttpError(404, f"Erreur lors de la récupération du produit : {e}")
+    except (requests.RequestException, ValueError, KeyError) as e:
+        raise HttpError(
+            status_code=404, message=f"Error when fetching the product data: {e}"
+        ) from e
