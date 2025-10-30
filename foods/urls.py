e670b150c9f2edfbabfc7ce485541431b91cb409
lanzac <a.lanrezac@gmail.com>
2025-10-30 16:50:53 +0000

    ♻️ refactor(urls): reorganize import statements and update urlpatterns syntax

--- foods/urls.py
+++ foods/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import (
-    FoodListView, FoodCreateView, FoodEditView, FoodDeleteView
-)
+from django.urls.resolvers import URLPattern
 
-urlpatterns = [
-    path('foods/', FoodListView.as_view(), name='list_foods'),
-    path('foods/new/', FoodCreateView.as_view(), name='create_food'),
-    path('foods/<str:pk>/edit/', FoodEditView.as_view(), name='edit_food'),
-    path('foods/<str:pk>/delete/', FoodDeleteView.as_view(), name='delete_food'),
+from .views import FoodCreateView
+from .views import FoodDeleteView
+from .views import FoodEditView
+from .views import FoodListView
+
+urlpatterns: list[URLPattern] = [
+    path(route="foods/", view=FoodListView.as_view(), name="list_foods"),
+    path(route="foods/new/", view=FoodCreateView.as_view(), name="create_food"),
+    path(route="foods/<str:pk>/edit/", view=FoodEditView.as_view(), name="edit_food"),
+    path(
+        route="foods/<str:pk>/delete/",
+        view=FoodDeleteView.as_view(),
+        name="delete_food",
+    ),
 ]
