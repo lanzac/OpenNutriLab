lanzac/OpenNutriLab

commit fd74d8c4369cc7abfdbed06b073b5d568059dd45

♻️ refactor(docker): remove Makefile in favor of justfile for container management

justfile
@@ -22,7 +22,12 @@ up:
 # down: Stop containers.
 down:
     @echo "Stopping containers..."
-    @docker compose down
+    @docker compose
+
+# restart: Restart containers.
+restart:
+    @echo "Restarting containers..."
+    @docker compose restart
 
 # prune: Remove containers and their volumes.
 prune *args:

Makefile
@@ -1,62 +1,0 @@
-# Variables
-DC=docker compose
-WEB=$(DC) run --rm web
-
-# 🔹 Docker Compose
-up:
-	$(DC) up --build
-
-up-detached:
-	$(DC) up -d
-
-down:
-	$(DC) down
-
-restart:
-	$(DC) down && $(DC) up
-
-build_web:
-	$(DC) build web
-
-# 🔹 Django Management
-migrate:
-	$(WEB) python manage.py migrate
-
-makemigrations:
-	$(WEB) python manage.py makemigrations
-
-createsuperuser:
-	$(WEB) python manage.py createsuperuser
-
-shell:
-	$(WEB) python manage.py shell
-
-dbshell:
-	$(WEB) python manage.py dbshell
-
-check:
-	$(WEB) python manage.py check
-
-collectstatic:
-	$(WEB) python manage.py collectstatic --noinput
-
-# 🔹 Tests
-test:
-	$(WEB) python manage.py test
-
-pytest:
-	$(WEB) pytest
-
-# Coverage
-coverage_test:
-	$(WEB) coverage run -m pytest
-
-coverage_report:
-	$(WEB) coverage report
-
-# 🔹 Custom Nutrient Init
-init_nutrients:
-	$(WEB) python manage.py init_nutrients
-
-load_nutrients:
-	$(WEB) python manage.py load_nutrients

commit cf74877cee4647f6bb7d46a4015b004c772fd6d9

♻️ refactor(justfile): fix down command to ensure containers stop correctly
- bug introduced in commit  fd74d8c4369cc7abfdbed06b073b5d568059dd45

justfile
@@ -22,7 +22,7 @@ up:
 # down: Stop containers.
 down:
     @echo "Stopping containers..."
-    @docker compose
+    @docker compose down
 
 # restart: Restart containers.
 restart: