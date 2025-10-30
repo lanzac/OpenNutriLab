6d46fd97298eb20cf612760abfd1c4f6885ca21a
lanzac <a.lanrezac@gmail.com>
2025-10-30 17:02:49 +0000

    ♻️ refactor(api): update add_router parameters for clarity
    - change add_router method parameters to use named arguments for better readability

--- opennutrilab/api.py
+++ opennutrilab/api.py
@@ -1,6 +1,7 @@
 # main_app/api.py
 from ninja import NinjaAPI
+
 from foods.api import router as foods_router
 
 api = NinjaAPI()
-api.add_router("/product/", foods_router)
+api.add_router(prefix="/product/", router=foods_router)
