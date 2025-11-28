c8f1171f49750fd1f0b5fd6e359f8706a831261d
lanzac <a.lanrezac@gmail.com>
2025-11-28 18:11:56 +0000

    ✨ feat(admin): register Ingredient model with hierarchical display in admin panel
    - add Ingredient model registration to the admin site
    - implement hierarchical display for ingredients using indented names

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from .models import Ingredient
+from .models import IngredientRef
 from .models import Macronutrient
 from .models import Product
 from .models import ProductMacronutrient
@@ -9,5 +11,36 @@ from .models import Vitamin
 # Register your models here.
 
 admin.site.register(
-    [Product, Macronutrient, ProductMacronutrient, Vitamin, ProductVitamin]
+    [
+        Product,
+        Macronutrient,
+        ProductMacronutrient,
+        Vitamin,
+        ProductVitamin,
+        IngredientRef,
+    ]
 )
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin[Ingredient]):
+    list_display = (
+        "indented_name",
+        "product",
+        "percentage",
+    )
+
+    ordering = ("id",)
+
+    @admin.display(description="Ingredient (hierarchical)")
+    def indented_name(self, obj: Ingredient) -> str:
+        indent = "— " * self.get_level(obj)
+        return f"{indent}{obj.name}"
+
+    def get_level(self, obj: Ingredient) -> int:
+        level = 0
+        parent = obj.parent
+        while parent:
+            level += 1
+            parent = parent.parent
+        return level
